0229b8c971bcdbbbca36a68d9b6fb155ed25717a
rahulchavare <115874595+rahulchavare@users.noreply.github.com>
2022-12-03 19:44:26 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -9,6 +9,8 @@ user="puser"
 pwd="pass123"
 driver="org.apache.postgre.sql"
 op_path="/user/hive/warehouse/dev.db/credit_card"
+op_path2="/user/hive/warehouse/dev.db/debit_card"
 df=spark.read.format("parquet").option("inferschema","true").load(input_path)
 df.write.format("csv").option("header","true").save(op_path)
-
+df_sql=spark.read.format("jdbc").option("url",host).option("username",user).option("password",pwd).option("driver",driver).option("dbtable","prod.debit_data").load()
+df_sql.write.format("parquet").option("header","true").save(op_path_2)
